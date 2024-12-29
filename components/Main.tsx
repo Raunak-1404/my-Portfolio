887fcf5ff40ca59d025602a993f60f1911a832fa
Raunak-1404 <jijotiaraunak14@gmail.com>
2024-12-29 18:06:44 +0530

    Completed the main page with Image and smooth transition

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { easeInOut, motion } from "framer-motion";
+import Image from "next/image";
 
+ 
 export default function MainPage() {
   return (
     <motion.div className="h-[89.8%] w-full text-white relative  tracking-[1px] ">
-      <motion.div className="absolute top-[30%] leading-[.9] left-[20%] ">
+      <motion.div className="absolute top-[38%] leading-[.9] left-[20%] z-50">
         <motion.h1
           initial={{ x: -2000, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
@@ -44,8 +46,53 @@ export default function MainPage() {
         </motion.h1>
       </motion.div>
 
-      <motion.div>
+      <motion.div className=" flex h-full justify-center relative">
           
+          <motion.div 
+          className="absolute left-[40%] "
+            initial={{opacity:0, x:150, rotate: 45}}
+            animate={{opacity:1, x:0, rotate:15}}
+            transition={{delay: 1.4, duration: 1}}
+          >
+            <Image 
+              className="object-cover z-0 " 
+              src="/IMG4.jpg"
+              alt="why is this not working"
+              width={500}
+              height={200}
+            />
+          </motion.div>
+
+          <motion.div 
+          className="absolute left-[39%] "
+            initial={{opacity:0, x:150, rotate: 45}}
+            animate={{opacity:1, x:0, rotate:10}}
+            transition={{delay: 2, duration: 1}}
+          >
+            <Image 
+              className="object-cover z-0 " 
+              src="/3.jpg"
+              alt="why is this not working"
+              width={500}
+              height={200}
+            />
+          </motion.div>
+
+          <motion.div 
+          className="absolute left-[40%] "
+            initial={{opacity:0, x:150, rotate: 45}}
+            animate={{opacity:1, x:0, rotate:0}}
+            transition={{delay: 2.5, duration: 1}}
+          >
+            <Image 
+              className="object-cover z-0 " 
+              src="/IMG1.jpg"
+              alt="why is this not working"
+              width={500}
+              height={200}
+            />
+          </motion.div>
+
       </motion.div>
     </motion.div>
   );

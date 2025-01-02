85e0f7b6cad741788ec8edc87e824bb1688dfeea
Raunak-1404 <jijotiaraunak14@gmail.com>
2025-01-02 13:59:17 +0530

    Added the Locomotive Scroll for smooth Scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
+'use client';
+import AboutPage from "@/components/AboutPage";
 import Frontpage from "@/components/Frontpage";
 import { div } from "framer-motion/client";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  async function SCROLL () {
+    const LocomotiveScroll = (await import('locomotive-scroll')).default;
+    const locomotiveScroll = new LocomotiveScroll(); 
+  }
+
+  useEffect(()=> {
+    SCROLL();
+  }, [])
+
   return (
     <>
       <Frontpage/>
+      <AboutPage/>
     </>
   );
 }
